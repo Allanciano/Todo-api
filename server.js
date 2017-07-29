f979f0be0b26c512e029f506309640aa32728944
Allan López <allanloji@gmail.com>
2017-07-29 17:52:32 -0500

    Final static todos

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ app.get('/todos',function(req, res){
     filteredTodos = _.where(filteredTodos, {completed: false});
   }
 
+  if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
+
   res.json(filteredTodos);
-
-
-  res.json(todos);
 });
 //GET /todos/:id
 app.get('/todos/:id',function(req, res){
